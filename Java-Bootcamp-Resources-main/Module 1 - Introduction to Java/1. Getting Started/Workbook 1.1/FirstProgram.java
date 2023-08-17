e8dff9a1aae1e3ed6d635bc79e11a14ab0eb2a8f
Uchirai <uchirai.govere@gmail.com>
2023-08-17 10:41:01 +0200

    random commit

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
@@ -1,2 +1,10 @@
 // I'm a comment. I have no impact on your code.
 // Instructions for this workbook are on Learn the Part (See the Udemy Video: Workbook 1.1 to access the link).
+class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("printing is fun");
+        System.out.println("java > python ");
+        System.out.println("I spilled java all over my paper");
+        System.out.println("my dog ate my java");
+    }
+}
