77764f5cf627fbce609d6e78648cd4bd7d3f87c6
Uchirai <uchirai.govere@gmail.com>
2023-08-18 11:23:26 +0200

    did exercise

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,10 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("U   U  GGGGG");
+         System.out.println("U   U  G    ");
+         System.out.println("U   U  G  GG");
+         System.out.println("U   U  G   G");
+         System.out.println("UUUUU  GGGGG");
     }
 }
